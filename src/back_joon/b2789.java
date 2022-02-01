16126393d18ac27d300c4ba503dadee8c8541aea
Donggyu Han <lucky5632@naver.com>
2022-02-01 22:06:21 +0900

    백준 2789번 - BruteForce 알고리즘

---
+++ src/back_joon/b2789.java
@@ -0,0 +1,33 @@
+package back_joon;
+import java.util.*;
+
+public class b2789 {
+    public static void main(String[] args) {
+        int N,M;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        int[] num = new int[N];
+
+        for(int i=0;i<N;i++){
+            num[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for(int i=0;i<N;i++){
+            for(int j=i+1; j<N ;j++){
+                for(int k=j+1; k<N ;k++){
+                    int sum = num[i] + num[j] + num[k];
+                    if(max < sum && sum <= M){
+                        max = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
