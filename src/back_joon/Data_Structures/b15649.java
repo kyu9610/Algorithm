1a01667edca6fa56bb1bb0afe2044bf89ae8cb3d
Donggyu Han <lucky5632@naver.com>
2022-02-15 13:16:34 +0900

    백준 15649번 문제 - 백트래킹

---
+++ src/back_joon/Data_Structures/b15649.java
@@ -0,0 +1,42 @@
+package back_joon.Data_Structures;
+import java.util.*;
+
+
+// BackTracking
+public class b15649 {
+    static int[] arr;
+    static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N,M,0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int N,int M,int depth){
+        if(depth == M){
+            for(int val : arr){
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            if(!visit[i]){
+                visit[i] = true;
+                arr[depth] = i+1;
+                dfs(N,M,depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
