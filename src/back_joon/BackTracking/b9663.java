70cb18803140e1752988229120b9d264ec30cd4f
Donggyu Han <lucky5632@naver.com>
2022-02-23 16:52:05 +0900

    백준 9663번문제 - 백트래킹

--- src/back_joon/BackTracking/b9663.java
+++ src/back_joon/BackTracking/b9663.java
@@ -1,11 +1,13 @@
-package back_joon.Data_Structures;
+package back_joon.BackTracking;
 import java.util.Scanner;
 
 // N-Queen 문제
 public class b9663 {
+
+    static int[] arr;
     static int N;
     static int count = 0;
-    static int[] arr;
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -14,36 +16,38 @@ public class b9663 {
         arr = new int[N];
 
         backTracking(0);
+
         System.out.println(count);
     }
 
     public static void backTracking(int depth){
-        if(depth == N){
+        if(N == depth){
             count++;
             return;
         }
 
         for(int i=0;i<N;i++){
             arr[depth] = i;
+
             if(possible(depth)){
                 backTracking(depth+1);
             }
         }
     }
 
-    public static boolean possible(int num){
-        for(int i=0;i<num;i++){
-            // 같은 행에 존재하는 경우
-            if(arr[num] == arr[i]){
+    public static boolean possible(int col){
+
+        for(int i=0;i<col;i++){
+            // 같은 행에 존재 여부 확인
+            if(arr[col] == arr[i]){
                 return false;
             }
 
-            // 대각선에 존재하는경우
-            else if(Math.abs(num-i) == Math.abs(arr[num] - arr[i])){
+            // 대각선 위치에 존재 여부 확인
+            else if(Math.abs(col-i) == Math.abs(arr[col] - arr[i])){
                 return false;
             }
         }
-
         return true;
     }
 }
