5bf7e88b3c5f07594f2866f551b97356472b9dfb
Donggyu Han <lucky5632@naver.com>
2022-03-06 13:06:21 +0900

    백준 11505번 문제 - 자료구조(세그먼트 트리)

--- src/back_joon/Data_Structures/b11505.java
+++ src/back_joon/Data_Structures/b11505.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class b11505 {
 
-    static int[] input;
-    static int[] tree;
+    static long[] input,tree;
     static int MOD = 1000000007;
 
     public static void main(String[] args) {
@@ -15,25 +14,25 @@ public class b11505 {
         int M = sc.nextInt();
         int K = sc.nextInt();
 
-        input = new int[N+1];
+        input = new long[N+1];
         for(int i=1;i<=N;i++){
-            input[i] = sc.nextInt();
+            input[i] = sc.nextLong();
         }
 
-        tree = new int[N*4];
+        tree = new long[N*4];
         init(1,N,1);
 
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<M+K;i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int c = sc.nextInt();
+            long c = sc.nextLong();
 
             if(a==1){
                 input[b] = c;
                 update(1,N,1,b,c);
             }else if(a==2){
-                sb.append(findMul(1,N,1,b,c)).append('\n');
+                sb.append(findMul(1,N,1,b,(int)c)).append('\n');
             }
         }
 
@@ -41,16 +40,16 @@ public class b11505 {
     }
 
 
-    public static int init(int start,int end,int node){
+    public static long init(int start,int end,int node){
         if(start == end){
             return tree[node] = input[start];
         }
 
         int mid = (start+end) / 2;
-        return tree[node] = (init(start,mid,node*2) * init(mid+1,end,node*2+1));
+        return tree[node] = (init(start,mid,node*2) * init(mid+1,end,node*2+1)) % MOD;
     }
 
-    public static int update(int start,int end,int node,int index,int val){
+    public static long update(int start,int end,int node,int index,long val){
         if(index < start || index > end){
             return tree[node];
         }
@@ -63,7 +62,7 @@ public class b11505 {
         return tree[node] = (update(start,mid,node*2,index,val) * update(mid+1,end,node*2+1,index,val)) % MOD;
     }
 
-    public static int findMul(int start,int end,int node,int left,int right){
+    public static long findMul(int start,int end,int node,int left,int right){
         if(left > end || right < start){
             return 1;
         }
