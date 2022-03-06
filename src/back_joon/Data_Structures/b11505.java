e33f36d64216d3653a8089ebe054e915650018b7
Donggyu Han <lucky5632@naver.com>
2022-03-06 12:56:20 +0900

    백준 11505번 문제 - 자료구조(세그먼트 트리)

---
+++ src/back_joon/Data_Structures/b11505.java
@@ -0,0 +1,79 @@
+package back_joon.Data_Structures;
+import java.util.*;
+
+public class b11505 {
+
+    static int[] input;
+    static int[] tree;
+    static int MOD = 1000000007;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int K = sc.nextInt();
+
+        input = new int[N+1];
+        for(int i=1;i<=N;i++){
+            input[i] = sc.nextInt();
+        }
+
+        tree = new int[N*4];
+        init(1,N,1);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<M+K;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if(a==1){
+                input[b] = c;
+                update(1,N,1,b,c);
+            }else if(a==2){
+                sb.append(findMul(1,N,1,b,c)).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+
+    public static int init(int start,int end,int node){
+        if(start == end){
+            return tree[node] = input[start];
+        }
+
+        int mid = (start+end) / 2;
+        return tree[node] = (init(start,mid,node*2) * init(mid+1,end,node*2+1));
+    }
+
+    public static int update(int start,int end,int node,int index,int val){
+        if(index < start || index > end){
+            return tree[node];
+        }
+
+        if(start == end){
+            return tree[node] = val;
+        }
+
+        int mid = (start+end) / 2;
+        return tree[node] = (update(start,mid,node*2,index,val) * update(mid+1,end,node*2+1,index,val)) % MOD;
+    }
+
+    public static int findMul(int start,int end,int node,int left,int right){
+        if(left > end || right < start){
+            return 1;
+        }
+
+        if(left <= start && end <= right){
+            return tree[node];
+        }
+
+        int mid = (start+end) / 2;
+
+        return (findMul(start,mid,node*2,left,right) * findMul(mid+1,end,node*2+1,left,right)) % MOD;
+    }
+}
