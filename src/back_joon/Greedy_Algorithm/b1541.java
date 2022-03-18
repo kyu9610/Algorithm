563bf26143bfcce50d69f357852ebe42261ca25b
Donggyu Han <lucky5632@naver.com>
2022-03-18 20:40:36 +0900

    백준 1541 - Greedy

---
+++ src/back_joon/Greedy_Algorithm/b1541.java
@@ -0,0 +1,29 @@
+package back_joon.Greedy_Algorithm;
+import java.util.*;
+
+public class b1541 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = Integer.MAX_VALUE;
+        String[] subtraction = sc.nextLine().split("-");
+
+        for(int i=0; i<subtraction.length;i++){
+            int temp = 0;
+
+            String[] addition = subtraction[i].split("\\+");
+
+            for(int j=0; j< addition.length;j++){
+                temp += Integer.parseInt(addition[j]);
+            }
+
+            if(sum == Integer.MAX_VALUE){
+                sum = temp;
+            }else{
+                sum -= temp;
+            }
+        }
+        System.out.println(sum);
+    }
+}
