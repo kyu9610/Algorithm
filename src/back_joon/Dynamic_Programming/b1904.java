a706cb20d13cac39d2b64770d36226a8a41a0c08
Donggyu Han <lucky5632@naver.com>
2022-04-27 19:42:04 +0900

    백준 1904 - DP

---
+++ src/back_joon/Dynamic_Programming/b1904.java
@@ -0,0 +1,34 @@
+package back_joon.Dynamic_Programming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b1904 {
+
+    static int dp[] = new int[1000001];
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3;i<dp.length;i++){
+            dp[i] = -1;
+        }
+
+        System.out.println(Tile(N));
+    }
+
+    public static int Tile(int N){
+
+        if(dp[N] == -1){
+            dp[N] = (Tile(N-1) + Tile(N-2)) % 15746;
+        }
+
+        return dp[N];
+    }
+}
