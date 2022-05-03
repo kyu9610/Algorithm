ccae20a5c6342dcf34451c8add9c72678bc7e9f4
Donggyu Han <lucky5632@naver.com>
2022-05-03 12:03:07 +0900

    백준 1978번 - 소수찾기

---
+++ src/back_joon/etc/b1978.java
@@ -0,0 +1,43 @@
+package back_joon.etc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b1978 {
+
+    static int[] num;
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine();	// N 은 쓰지 않음.
+        int count = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        while(st.hasMoreTokens()) {
+
+            // 소수인경우 true, 아닌경우 false
+            boolean isPrime = true;
+
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num == 1) {
+                continue;
+            }
+            for(int i = 2; i <= Math.sqrt(num); i++) {
+                if(num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
