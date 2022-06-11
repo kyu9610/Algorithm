eb577b636b81f25714e8193f4265614827060a48
Donggyu Han <lucky5632@naver.com>
2022-06-11 22:47:06 +0900

    프로그래머스 - 타겟 넘버 (bfs)

---
+++ src/programmers/target.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class target {
+    static int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers,0,target,0);
+        return answer;
+    }
+
+    public static void dfs(int[] numbers,int depth,int target,int sum){
+        if(depth == numbers.length){
+            if(target == sum) answer++;
+        }else{
+            dfs(numbers, depth+1, target, sum + numbers[depth]);
+            dfs(numbers, depth+1, target, sum - numbers[depth]);
+        }
+    }
+}
