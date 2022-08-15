02aa6ea57e1f7f0612874228bcc571345dbda87c
Donggyu Han <lucky5632@naver.com>
2022-08-15 17:08:00 +0900

    백준 10816 - 이분탐색의 기본

---
+++ src/back_joon/BinarySearch/b10816.java
@@ -0,0 +1,72 @@
+package back_joon.BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class b10816 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<M;i++){
+            int key = Integer.parseInt(st.nextToken());
+
+            sb.append(upperBound(arr,key) - lowerBound(arr,key)).append(' ');
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static int lowerBound(int[] arr,int key){
+        int lo = 0;
+        int hi = arr.length;
+
+        while(lo < hi){
+            int mid = (lo + hi) / 2;
+
+
+
+            if(key <= arr[mid]){
+                hi = mid;
+            }else{
+                lo = mid+1;
+            }
+        }
+
+        return lo;
+    }
+
+    private static int upperBound(int[] arr,int key){
+        int lo = 0;
+        int hi = arr.length;
+
+        while(lo < hi){
+            int mid = (lo+hi) / 2;
+
+            if(key < arr[mid]){
+                hi = mid;
+            }else{
+                lo = mid+1;
+            }
+        }
+
+        return lo;
+    }
+}
